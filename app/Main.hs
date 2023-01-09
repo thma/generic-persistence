d5be8f1a2b9ad2025ae0051f822f080523765eda
thma <gH1710>
2023-01-09 21:36:56 +0100

    stackoverflow rescued me, again!

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,36 @@
 {-# LANGUAGE DeriveDataTypeable#-}
 {-# LANGUAGE ExtendedDefaultRules#-}
+{-# LANGUAGE GADTs #-}
 module Main (main) where
 
-import Data.Data
+import Data.Data hiding (typeRep, IntRep, TypeRep)
 
 import TypeInfo
 import SqlGenerator
---import Data.List (intercalate, elemIndex)
-import Data.Dynamic (toDyn)
---import Control.Monad.Trans.State.Lazy
---import Control.Monad.Trans.Class (lift)
+import Data.Dynamic --(toDyn)
+import Type.Reflection
+--import GHC.Base (RuntimeRep(..))
+import GHC.Data.Maybe
+import Control.Monad
+import GHC.Types
+
+-- https://stackoverflow.com/questions/46992740/how-to-specify-type-of-value-via-typerep
+types :: [SomeTypeRep]
+types = [SomeTypeRep (typeRep @Int), SomeTypeRep (typeRep @Double)]
+
+strings :: [String]
+strings = ["34", "76876.89"]
+
+converted :: [Dynamic]
+converted = expectJust "casting not possible" $ zipWithM convert types strings
+
+convert :: SomeTypeRep -> String -> Maybe Dynamic
+convert (SomeTypeRep rep) str
+  | Just HRefl <- eqTypeRep rep (typeRep @Int)    = Just $ toDyn (read str :: Int)
+  | Just HRefl <- eqTypeRep rep (typeRep @Double) = Just $ toDyn (read str :: Double)
+  | Just HRefl <- eqTypeRep rep (typeRep @String) = Just $ toDyn str
+  | otherwise = Nothing
+
 
 
 -- Define a sample data type
@@ -54,12 +75,21 @@ main = do
   putStrLn $ updateStmtFor p
   putStrLn $ deleteStmtFor p
 
-  let bob = applyConstr (toConstr p) [toDyn 4711, toDyn "Bob", toDyn 30, toDyn "456 Main St"] :: Maybe Person
+  let types :: [SomeTypeRep]
+      types = [SomeTypeRep (typeRep @Int), SomeTypeRep (typeRep @String), SomeTypeRep (typeRep @Int), SomeTypeRep (typeRep @String)]
+      strings :: [String]
+      strings = ["4712", "Bob Marley", "36", "Tuff Gong Studio, Kingston, Jamaica"]
+      converted :: [Dynamic]
+      converted = expectJust "casting not possible" $ zipWithM convert types strings
+
+      bob = applyConstr (toConstr p) [toDyn (4711 :: Int), toDyn "Bob", toDyn (30 :: Int), toDyn "456 Main St"] :: Maybe Person
       rob = applyConstr (toConstr p) [toDyn (read "5656" :: Int), toDyn "Rob", toDyn (31 :: Int), toDyn "457 Main St"] :: Maybe Person
+      bobM = applyConstr (toConstr p) converted :: Maybe Person
       x = fromConstrB (fromConstr (toConstr 6879879)) (toConstr (Just 1 )) :: Maybe Int
 
   print bob
   print rob
+  print bobM
   print x
   let ctor = toConstr p
       dtype = constrType ctor
