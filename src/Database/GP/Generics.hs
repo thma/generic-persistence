e355758d8b136dc37c4f815c2821693f7add318f
Thomas Mahler <thomas.mahler@ista.com>
2023-02-17 17:25:27 +0100

    The friendly people on stackoverflow helped me to get this working. Fimally!

--- src/Database/GP/Generics.hs
+++ src/Database/GP/Generics.hs
@@ -1,23 +1,14 @@
-{-# Language 
-  DefaultSignatures, 
-  FlexibleContexts, 
-  DeriveAnyClass,
-  StandaloneDeriving #-}
-module Database.GP.Generics(
-  ToRow,
-  toRow,
-  GToRow,
-  gtoRow,
-  ToSql,
-  toSqlValue,
-  main,
-) where
+{-# LANGUAGE DataKinds, TypeFamilies, TypeOperators, UndecidableInstances, ScopedTypeVariables, DefaultSignatures, DeriveAnyClass #-}
+
+module Database.GP.Generics (main) where
 
 import GHC.Generics
-import Database.HDBC
---import Database.HDBC.SqlValue
-import Data.Int
-import Data.Word
+import Database.HDBC ( fromSql, toSql, SqlValue )
+import Data.Convertible ( convert, ConvertResult, Convertible(..) )
+import Data.Kind
+import GHC.TypeNats
+import Data.Proxy
+
 
 class ToRow a where
   toRow :: a -> [SqlValue]
@@ -26,13 +17,13 @@ class ToRow a where
   toRow a = gtoRow $ from a
 
 class GToRow f where
-  gtoRow :: (f a) -> [SqlValue]
+  gtoRow :: f a -> [SqlValue]
 
 instance GToRow U1 where
   gtoRow U1 = mempty
 
-instance ToSql a => GToRow (K1 i a) where
-  gtoRow (K1 a) = pure $ toSqlValue a
+instance (Convertible a SqlValue) => GToRow (K1 i a) where
+  gtoRow (K1 a) = pure $ convert a
 
 instance (GToRow a, GToRow b) => GToRow (a :*: b) where
   gtoRow (a :*: b) = gtoRow a `mappend` gtoRow b
@@ -40,115 +31,73 @@ instance (GToRow a, GToRow b) => GToRow (a :*: b) where
 instance GToRow a => GToRow (M1 i c a) where
   gtoRow (M1 a) = gtoRow a
 
--- | A type that may be used as a single parameter to a SQL query.
-class ToSql a where
-    toSqlValue :: a -> SqlValue
+----
 
-instance ToSql SqlValue where
-    toSqlValue a = a
-    {-# INLINE toSqlValue #-}
+class FromRow a where
+  fromRow :: [SqlValue] -> a
 
--- instance (ToSql a) => ToSql (Maybe a) where
---     toSqlValue Nothing  = Base.SQLNull
---     toSqlValue (Just a) = toSqlValue a
---     {-# INLINE toSqlValue #-}
+  default fromRow :: Generic a => GFromRow (Rep a) => [SqlValue] -> a
+  fromRow = to <$> gfromRow
 
--- instance ToSql Null where
---     toSqlValue _ = Base.SQLNull
---     {-# INLINE toSqlValue #-}
 
-instance ToSql Bool where
-    toSqlValue False = SqlInteger 0
-    toSqlValue True  = SqlInteger 1
-    {-# INLINE toSqlValue #-}
+class GFromRow f where
+  gfromRow :: [SqlValue] -> f a
 
-instance ToSql Int8 where
-    toSqlValue = SqlInteger . fromIntegral
-    {-# INLINE toSqlValue #-}
+instance GFromRow U1 where
+  --gfromRow :: forall k (a :: k). [SqlValue] -> U1 a
+  gfromRow = pure U1
 
-instance ToSql Int16 where
-    toSqlValue = SqlInteger . fromIntegral
-    {-# INLINE toSqlValue #-}
+instance (Convertible SqlValue a) => GFromRow (K1 i a) where
+  --gfromRow :: forall k (a1 :: k). [SqlValue] -> K1 i a a1
+  gfromRow = K1 <$> convert . head
 
-instance ToSql Int32 where
-    toSqlValue = SqlInteger . fromIntegral
-    {-# INLINE toSqlValue #-}
+instance GFromRow a => GFromRow (M1 i c a) where
+  --gfromRow :: forall k (a :: k -> Type) i (c :: Meta) (a1 :: k). GFromRow a => [SqlValue] -> M1 i c a a1
+  gfromRow = M1 <$> gfromRow
 
-instance ToSql Int where
-    toSqlValue = SqlInteger . fromIntegral
-    {-# INLINE toSqlValue #-}
+-- | This instance is the most interesting one. It splits the list of
+-- 'SqlValue's into two parts, one for the first field and one for the
+-- rest. Then it uses the 'GFromRow' instance for the first field to
+-- convert the first part of the list and the 'GFromRow' instance for
+-- the rest of the fields to convert the second part of the list.
+-- Finally, it combines the two results using the ':*:' constructor.
+-- https://stackoverflow.com/questions/75485429/how-to-use-ghc-generics-to-convert-from-product-data-types-to-a-list-of-sqlvalue/75485650#75485650
+instance (KnownNat (NumFields f), GFromRow f, GFromRow g) => GFromRow (f :*: g) where
+  gfromRow row = gfromRow rowf :*: gfromRow rowg
+    where
+      (rowf, rowg) = splitAt fNumFields row
+      fNumFields = fromIntegral (natVal (Proxy :: Proxy (NumFields f)))
 
-instance ToSql Int64 where
-    toSqlValue = SqlInteger . fromIntegral
-    {-# INLINE toSqlValue #-}
+type family NumFields (f :: Type -> Type) :: Nat where
+  NumFields (M1 i c f) = 1
+  NumFields (f :*: g) = NumFields f + NumFields g
 
-instance ToSql Integer where
-    toSqlValue = SqlInteger
-    {-# INLINE toSqlValue #-}
 
-instance ToSql Word8 where
-    toSqlValue = SqlInteger . fromIntegral
-    {-# INLINE toSqlValue #-}
 
-instance ToSql Word16 where
-    toSqlValue = SqlInteger . fromIntegral
-    {-# INLINE toSqlValue #-}
-
-instance ToSql Word32 where
-    toSqlValue = SqlInteger . fromIntegral
-    {-# INLINE toSqlValue #-}
-
-instance ToSql Word where
-    toSqlValue = SqlInteger . fromIntegral
-    {-# INLINE toSqlValue #-}
-
-instance ToSql Word64 where
-    toSqlValue = SqlInteger . fromIntegral
-    {-# INLINE toSqlValue #-}
-
-instance ToSql Float where
-    toSqlValue = SqlDouble . realToFrac
-    {-# INLINE toSqlValue #-}
-
-instance ToSql Double where
-    toSqlValue = SqlDouble
-    {-# INLINE toSqlValue #-}
-
--- instance ToSql SB.ByteString where
---     toSqlValue = SQLBlob
---     {-# INLINE toSqlValue #-}
-
--- instance ToSql LB.ByteString where
---     toSqlValue = toSqlValue . SB.concat . LB.toChunks
---     {-# INLINE toSqlValue #-}
-
--- instance ToSql T.Text where
---     toSqlValue = SQLText
---     {-# INLINE toSqlValue #-}
-
-instance ToSql String where
-    toSqlValue = SqlString
-    {-# INLINE toSqlValue #-}
-
--- instance ToSql LT.Text where
---     toSqlValue = toSqlValue . LT.toStrict
---     {-# INLINE toSqlValue #-}
-
--- instance ToSql UTCTime where
---     toSqlValue = SQLText . T.decodeUtf8 . toByteString . utcTimeToBuilder
---     {-# INLINE toSqlValue #-}
-
--- instance ToSql Day where
---     toSqlValue = SQLText . T.decodeUtf8 . toByteString . dayToBuilder
---     {-# INLINE toSqlValue #-}
-
-data Person = Person 
-  { name :: String, 
-    age :: Int 
+data Person = Person
+  { name :: String,
+    age :: Int,
+    role :: Role
   }
-    deriving (Generic, Show, ToRow)
+    deriving (Generic, Show, ToRow, FromRow)
+
+
+data Role = Student | Teacher
+  deriving (Generic, Show, Read, Enum)
+
+instance Convertible Role SqlValue where
+  safeConvert :: Role -> ConvertResult SqlValue
+  safeConvert = Right . toSql . fromEnum
+
+instance Convertible SqlValue Role where
+  safeConvert :: SqlValue -> ConvertResult Role
+  safeConvert = Right . toEnum . fromSql
+
 
 main :: IO ()
 main = do
-    let sqlValueList = toRow $ Person "John Doe" 30
+    let sqlValueList = toRow $ Person "John Doe" 30 Teacher
     putStrLn $ "SQL values: " ++ show sqlValueList
+
+    let person = fromRow sqlValueList :: Person
+    putStrLn $ "Person: " ++ show person
